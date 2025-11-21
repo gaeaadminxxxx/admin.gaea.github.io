1fcab890d3fc34f621395bf89206312d971f48c8
gaea3 <develop@aigaea.net>
2025-11-21 09:58:41 +0000

    update 2025-11-21 09:58:41

---
+++ static/redirect.1d3d078c.js
@@ -0,0 +1,1 @@
+import{c as e,o as r}from"./entry.92ba0b18.js";const _={__name:"redirect",setup(t){return(c,o)=>(r(),e("div"))}};export{_ as default};
---
+++ static/redirect.e451ed07.js
@@ -0,0 +1,1 @@
+import{aq as a,V as r,a8 as s}from"./entry.92ba0b18.js";const u=a((o,e)=>{const t=r();if(e.query.token)return t.setToken(e.query.token),s(`/${e.query.path}`)});export{u as default};
